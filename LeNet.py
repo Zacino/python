c5da1302a37eed03758672e35f52ad6825750335
win <1423485056@qq.com>
2024-04-11 11:32:28 +0800

    test

---
+++ LeNet.py
@@ -0,0 +1,10 @@
+import numpy as np
+import torch
+from torch.utils import data
+from d2l import torch as d2l
+
+true_w = torch.tensor([2, -3.4])
+true_b = 4.2
+features, labels = d2l.synthetic_data(true_w, true_b, 1000)
+
+print(true_w)
