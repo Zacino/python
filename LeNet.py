a85b5285a291175253900d8c73716e17bdcd059d
win <1423485056@qq.com>
2024-04-13 10:48:30 +0800

    LeNet

--- LeNet.py
+++ LeNet.py
@@ -1,10 +1,95 @@
-import numpy as np
 import torch
-from torch.utils import data
+from torch import nn
 from d2l import torch as d2l
 
-true_w = torch.tensor([2, -3.4])
-true_b = 4.2
-features, labels = d2l.synthetic_data(true_w, true_b, 1000)
+net = nn.Sequential(
+    nn.Conv2d(1, 6, kernel_size=5, padding=2),  # 卷积层 四维
+    nn.Sigmoid(),
+    nn.AvgPool2d(kernel_size=2, stride=2),
+    nn.Conv2d(6, 16, kernel_size=5),
+    nn.Sigmoid(),
+    nn.AvgPool2d(kernel_size=2, stride=2),
+    nn.Flatten(),  # 拉成一维向量，默认从第二维开始，第一维度是batch
+    nn.Linear(16 * 5 * 5, 120),  # 全连接层 二位张量 [batch_size, size]
+    nn.Sigmoid(),
+    nn.Linear(120, 84),
+    nn.Sigmoid(),
+    nn.Linear(84, 10))
 
-print(true_w)
+
+# [batch_size, channel, height, width] 的输入张量x
+# batch_size 是一次训练样本个数，对于训练集，一次epoch要训练 total/batch_size 次
+batch_size = 25  # 每次训练样本个数
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size=batch_size)
+'''
+dataloader 搭配 enumerate
+
+'''
+
+
+
+
+def evaluate_accuracy_gpu(net, data_iter, device=None): #@save
+    """使用GPU计算模型在数据集上的精度"""
+    if isinstance(net, nn.Module):
+        net.eval()  # 设置为评估模式
+        if not device:
+            device = next(iter(net.parameters())).device
+    # 正确预测的数量，总预测的数量
+    metric = d2l.Accumulator(2)
+    with torch.no_grad():
+        for X, y in data_iter:
+            if isinstance(X, list):
+                # BERT微调所需的（之后将介绍）
+                X = [x.to(device) for x in X]
+            else:
+                X = X.to(device)
+            y = y.to(device)
+            metric.add(d2l.accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
+#@save
+def train_ch6(net, train_iter, test_iter, num_epochs, lr, device):
+    """用GPU训练模型(在第六章定义)"""
+    def init_weights(m):
+        if type(m) == nn.Linear or type(m) == nn.Conv2d:
+            nn.init.xavier_uniform_(m.weight)
+    net.apply(init_weights)
+    print('training on', device)
+    net.to(device)
+    optimizer = torch.optim.SGD(net.parameters(), lr=lr)
+    loss = nn.CrossEntropyLoss()
+    animator = d2l.Animator(xlabel='epoch', xlim=[1, num_epochs],
+                            legend=['train loss', 'train acc', 'test acc'])
+    timer, num_batches = d2l.Timer(), len(train_iter)
+    for epoch in range(num_epochs):
+        # 训练损失之和，训练准确率之和，样本数
+        metric = d2l.Accumulator(3)
+        net.train()
+        for i, (X, y) in enumerate(train_iter):
+            timer.start()
+            optimizer.zero_grad()
+            X, y = X.to(device), y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            l.backward()
+            optimizer.step()
+            with torch.no_grad():
+                metric.add(l * X.shape[0], d2l.accuracy(y_hat, y), X.shape[0])
+            timer.stop()
+            train_l = metric[0] / metric[2]
+            train_acc = metric[1] / metric[2]
+            if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
+                animator.add(epoch + (i + 1) / num_batches,
+                             (train_l, train_acc, None))
+        test_acc = evaluate_accuracy_gpu(net, test_iter)
+        animator.add(epoch + 1, (None, None, test_acc))
+    # print(f'loss {train_l:.3f}, train acc {train_acc:.3f}, '
+    #       f'test acc {test_acc:.3f}')
+    # print(f'{metric[2] * num_epochs / timer.sum():.1f} examples/sec '
+    #       f'on {str(device)}')
+
+
+lr, num_epochs = 0.9, 10
+train_ch6(net, train_iter, test_iter, num_epochs, lr, d2l.try_gpu())
---
+++ grammer.py
@@ -0,0 +1,14 @@
+import torch
+from torch.utils.data import DataLoader
+
+
+class MyDataLoader(torch.utils.data.Dataset):
+    def __init__(self, data, label):
+        self.data = data
+        self.label = label
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, item) :
+        return self.data[item], self.label[item]
